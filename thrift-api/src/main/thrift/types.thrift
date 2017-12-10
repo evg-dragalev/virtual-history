97318bc281e3c63dca2ef2785b0000f2e491bfc0
Evgeniy Dragalev <evg.dragalev@gmail.com>
2017-12-11 00:55:52 +0300

    Add main fields for structs

--- thrift-api/src/main/thrift/types.thrift
+++ thrift-api/src/main/thrift/types.thrift
@@ -1,20 +1,45 @@
 namespace java com.virtualhistory.services.structs
 
-struct Quest{
-	
-}
-
 struct Location {
     1: required double latitude;
     2: required double longitude;
 }
 
-struct Museum{
+struct QuestStage{
+    1: required string questId;
+    2: optional Location completePoint;
+    3: optional QuestStage nextStage;
+    4: optional string description;
+    5: optional i64 timer;
+    //
+}
 
+struct Quest{
+	1: required string id;
+	2: required Location startPoint;
+    3: optional list<QuestStage> stages;
+    4: optional string description;
+    5: optional i32 peopleEntered;
+    6: optional i16 rating;
+    //
+}
+
+struct Museum{
+    1: required string id;
+    2: required string name;
+    3: required Location location;
+    4: optional string description;
+    //
 }
 
 struct User{
-
+    1: required string id;
+    2: required string username;
+    3: required list<Quest> passedQuests;
+    4: optional list<Quest> bookmarkedQuests;
+    //
 }
 
+struct Model{
 
+}
