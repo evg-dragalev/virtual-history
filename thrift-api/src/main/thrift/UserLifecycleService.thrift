f762d67a11bfedf3783f171fbd8d3872e9f02829
Eugene Motolov <zheniamotolov@gmail.com>
2017-12-27 16:51:56 +0300

    added new services

--- thrift-api/src/main/thrift/UserLifecycleService.thrift
+++ thrift-api/src/main/thrift/UserLifecycleService.thrift
@@ -1,25 +1,30 @@
 namespace java com.virtualhistory.services
 include "types.thrift"
 
-service UserLifecycleService.thrift{
+service UserLifecycleService{
         string CreateUser(
         1: string  Username;
         2: string Password;
         3: string Email;
         4: string Phone
-        )
+        )throws (
+         		)
         string LogIn(
         1: string Username;
         2: string Password;
-        )
+        )throws (
+         		)
         string EditUserInfo(
          1: string  Username;
          2: string Password;
          3: string Email;
          4: string Phone
-        )
+        )throws (
+
+         		)
         string DeleteUser(
         1: string Username;
         2: string Password;
-        )
+        )throws (
+         		)
 }
---
+++ thrift-api/src/main/thrift/VisualDataAnylisis.thrift
@@ -0,0 +1,16 @@
+namespace java com.virtualhistory.services
+include "types.thrift"
+
+service UserLifecycleService{
+      string MahoutPreferenceAnalysis(
+        1: list<User>  UserData;
+        2: list<UserPreference> UserPreferenceData
+        )throws (
+         		)
+       string SocialMediaAnalysis(
+       1: list<Messages> socciaMedia
+       2: list<Prioritets> SocialPrioritets
+       )
+
+
+}
